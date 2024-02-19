70c23ff542cd85bc7a4fab595b18bc2017606d58
Jiahao XU <Jiahao_XU@outlook.com>
2024-02-20 00:15:55 +1100

    Add README
    Rm `/target` and add `.gitignore`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- target/debug/build/proc-macro2-6de174ccb7206bfd/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-/Users/nobodyxu/Dev/leon/target/debug/build/proc-macro2-6de174ccb7206bfd/out/libproc_macro2.rmeta: build/probe.rs
-
-/Users/nobodyxu/Dev/leon/target/debug/build/proc-macro2-6de174ccb7206bfd/out/proc_macro2.d: build/probe.rs
-
-build/probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- target/debug/deps/unicode_width-ae1ffe84cbe7b16b.d
+++
@@ -1,6 +0,0 @@
-/Users/nobodyxu/Dev/leon/target/debug/deps/libunicode_width-ae1ffe84cbe7b16b.rmeta: /Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/lib.rs /Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/tables.rs
-
-/Users/nobodyxu/Dev/leon/target/debug/deps/unicode_width-ae1ffe84cbe7b16b.d: /Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/lib.rs /Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/tables.rs
-
-/Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/lib.rs:
-/Users/nobodyxu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.11/src/tables.rs:
